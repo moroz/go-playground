5a2cc7f9cae638241a5ad2f19dee7ee3f63a6d0e
Karol Moroz <k.j.moroz@gmail.com>
2022-12-02 23:30:17 +0100

    Refactor functionality into a function

--- wordcount/main.go
+++ wordcount/main.go
@@ -8,30 +8,38 @@ import (
 	"strings"
 )
 
+func countWordsInFile(filename string) (int, error) {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var wordCount int
+
+	// for functions just like while in C
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		wordCount += len(words)
+	}
+
+	return wordCount, scanner.Err()
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Println("need to provide filename!")
 		os.Exit(1)
 	}
 
-	file, err := os.Open(os.Args[1])
+	wordCount, err := countWordsInFile(os.Args[1])
+
+	// This will be not nil if scanner.Scan() returned early
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	var wordCount int
-
-	for scanner.Scan() {
-		words := strings.Fields(scanner.Text())
-		wordCount += len(words)
-	}
-
-	if scanner.Err() != nil {
-		log.Println(scanner.Err())
-		return
 	}
 
 	fmt.Println("Found", wordCount, "words")
