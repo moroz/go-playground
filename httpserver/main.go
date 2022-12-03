85b66d407533da78402b49a38f16dbf59da0dfd5
Karol Moroz <k.j.moroz@gmail.com>
2022-12-03 13:24:45 +0100

    Use a separate multiplexer, get port from env

--- httpserver/main.go
+++ httpserver/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -19,11 +20,25 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
-func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+const DEFAULT_PORT string = ":4000"
 
-	err := http.ListenAndServe(":3333", nil)
+func getPort() string {
+	port := os.Getenv("PORT")
+	portNumber, err := strconv.Atoi(port)
+	if err != nil {
+		return DEFAULT_PORT
+	}
+	return fmt.Sprintf(":%d", portNumber)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	port := getPort()
+	log.Printf("Listening on port %s\n", port)
+	err := http.ListenAndServe(":3333", mux)
 	if errors.Is(err, http.ErrServerClosed) {
 		log.Printf("server closed\n")
 	} else if err != nil {
