7aeb948a58651501cbbb08b1b9e2327d3e70707b
Karol Moroz <k.j.moroz@gmail.com>
2022-12-03 15:11:36 +0100

    Connect to PG and execute raw query

--- chi_demo/go.mod
+++ chi_demo/go.mod
@@ -2,4 +2,7 @@ module moroz/chi_demo
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
--- chi_demo/main.go
+++ chi_demo/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	connStr := "postgres://postgres:postgres@localhost:5432/url_shortener_dev?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := db.Query("Select now() at time zone 'utc';")
+	if err != nil {
+		log.Printf("Could not execute query: %s\n", err)
+	} else {
+		var rows []string
+		var row string
+
+		for result.Next() {
+			result.Scan(&row)
+			fmt.Println(row)
+			rows = append(rows, row)
+		}
+
+		if result.Err() != nil {
+			fmt.Println(result.Err())
+		}
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
